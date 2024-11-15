2a2fb2e119ea4456f0c34295462b1b526d77fa8c
cediazz <dce92179@gmail.com>
2024-11-14 19:27:56 -0500

    OverwriteCreateMethodUserSerializer

--- user_management/serializers.py
+++ user_management/serializers.py
@@ -5,3 +5,9 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id','username','password','image',]
+    
+    def create(self,validated_data):
+        user = CustomUser(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
