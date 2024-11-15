386850ddd8c7d5982edf8a88966e3f28443a27bd
cediazz <dce92179@gmail.com>
2024-11-15 16:03:08 -0500

    TokenVerifyView

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     path('login/',MyTokenObtainPairView.as_view()),
     path('refresh-token/',TokenRefreshView.as_view()),
+    path('token-verify/', TokenVerifyView.as_view()),
 ]
